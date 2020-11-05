cddf624c8b405bfcc6263ecd29b856ce99a4c659
minhozx <minhozx@skku.edu>
2020-11-05 19:35:38 +0900

    Update pupil.py

--- gaze_tracking/pupil.py
+++ gaze_tracking/pupil.py
@@ -30,6 +30,9 @@ class Pupil(object):
             A frame with a single element representing the iris
         """
         kernel = np.ones((3, 3), np.uint8)
+        """
+		add a frame
+        """
         new_frame = cv2.bilateralFilter(eye_frame, 10, 15, 15)
         new_frame = cv2.erode(new_frame, kernel, iterations=3)
         new_frame = cv2.threshold(new_frame, threshold, 255, cv2.THRESH_BINARY)[1]
